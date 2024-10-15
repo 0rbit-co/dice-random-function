c25df47517ad926ff2fe9e84435e5e9786ea7ebe
Not-Sarthak <notsarthakshah@gmail.com>
2024-10-15 19:25:07 +0530

    feat: blog link

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ConnectButton } from "arweave-wallet-kit";
 import "./App.css";
 import EnhancedDiceGame from "./components/number";
@@ -7,6 +8,16 @@ function App() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-gray-100 to-gray-300">
       <ConnectButton profileModal={true} showBalance={true} className="mb-8" />
       <EnhancedDiceGame />
+      <div className="mt-8 text-center">
+        <a 
+          href="https://0rbit.hashnode.dev/dice-game-with-0rbit" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="textColor"
+        >
+          Check out the blog post here!
+        </a>
+      </div>
     </div>
   );
 }
